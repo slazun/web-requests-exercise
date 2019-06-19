b426817bc04d23055101d6d1da2c7f335ff63dfd
Sarah Lazun <sel402@stern.nyu.edu>
2019-06-19 18:48:10 -0400

    get json data from internet

--- get_data.py
+++ get_data.py
@@ -1,1 +1,7 @@
+import json
+import requests
 print("REQUESTING SOME DATA FROM THE INTERNET...")
+request_url = "https://raw.githubusercontent.com/prof-rossetti/nyu-info-2335-201905/master/data/products/2.json"
+response = requests.get(request_url)
+print(response.status_code)
+print(response.text)
