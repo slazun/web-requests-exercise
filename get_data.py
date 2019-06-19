5a6bfbbff555cc01302e76c53879a5fe6d6ea8ef
Sarah Lazun <sel402@stern.nyu.edu>
2019-06-19 19:03:44 -0400

    printing json file contents

--- get_data.py
+++ get_data.py
@@ -3,5 +3,8 @@ import requests
 print("REQUESTING SOME DATA FROM THE INTERNET...")
 request_url = "https://raw.githubusercontent.com/prof-rossetti/nyu-info-2335-201905/master/data/products/2.json"
 response = requests.get(request_url)
-print(response.status_code)
-print(response.text)
+#print(response.status_code)
+#print(response.text)
+response_data = json.loads(response.text)
+print(type(response_data))
+print(response_data)
