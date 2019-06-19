97cef5ba08fdda2f54ebbde2fa98012bbfb53887
Sarah Lazun <sel402@stern.nyu.edu>
2019-06-19 19:55:45 -0400

    pulling form lists

--- get_data.py
+++ get_data.py
@@ -1,11 +1,20 @@
 import json
 import requests
 print("REQUESTING SOME DATA FROM THE INTERNET...")
-request_url = "https://raw.githubusercontent.com/prof-rossetti/nyu-info-2335-201905/master/data/products/2.json"
+request_url = "https://raw.githubusercontent.com/prof-rossetti/nyu-info-2335-201905/master/data/products.json"
 response = requests.get(request_url)
 #print(response.status_code)
 #print(response.text)
 response_data = json.loads(response.text)
 print(type(response_data))
 print(response_data)
-print(response_data['name'])
+#print(response_data['name'])
+
+#for product in response_data:
+#print(str(product['name']) + str(product['id'])
+
+#for product in response_data:
+    #print(product)
+
+for product in response_data:
+    print(str(product['name']) + " " + str(product['id']))
